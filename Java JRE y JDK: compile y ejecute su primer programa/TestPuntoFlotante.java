2e93297321322276c55e142feb5ebbe0163d7489
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-22 15:50:12 -0600

    Update TestPuntoFlotante.java

--- Java JRE y JDK: compile y ejecute su primer programa/TestPuntoFlotante.java
+++ Java JRE y JDK: compile y ejecute su primer programa/TestPuntoFlotante.java
@@ -6,25 +6,25 @@ public class TestPuntoFlotante {
 		double salario = 1205.70;
 		//System.out.println ("mi salario es $" + salario);
 		
-		/* Recuerda que no podemos colocar 
+		/*Recuerda que no podemos colocar 
 		 un valor de tipo flotante dentro 
-		 de una variable de tipo int, prueba:
-		
-		int valor = 12.5; //No compila por no ser flotante
-		
-		int valor2 = 0.0 //No compila
-		
-		double test = 125.5;	//Compila
-		
-		int valor3 = test; //No compila */
+		 de una variable de tipo int, prueba:*/
+			
+			int valor = 12.5; //No compila 
+			
+			int valor2 = 0.0 //No compila
+			
+			double test = 125.5;	//Compila
+			
+			int valor3 = test; //No compila 
 		
 		 //cálculo de división
 		
-		/* int division = 5/2;
-		System.out.println (division); //Nuestra salida será la 2.
-		*/
+		 int division = 5/2;
+			System.out.println (division); //Nuestra salida será la 2.
+		
 		double division = 5.0 / 2;
-				System.out.println(division);
+				System.out.println(division); //Nuestra salida será la 2.5
 	}
 	
 }
