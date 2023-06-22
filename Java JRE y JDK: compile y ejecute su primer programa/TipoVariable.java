a8b6efc0351247a1d5e5f27ea096271fe25f71ae
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-22 15:30:18 -0600

    Update TipoVariable.java

--- Java JRE y JDK: compile y ejecute su primer programa/TipoVariable.java
+++ Java JRE y JDK: compile y ejecute su primer programa/TipoVariable.java
@@ -1,12 +1,20 @@
 
 public class TipoVariable {
-	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
+
+		//Esta es una variable de tipo entero. 
 		int edad = 25;
 		System.out.println("Mi edad es " + edad);
 		
-	
+		// Este es un tipo de datos double
+		double salario = 1250.50;
+		System.out.println("El salario es $" + salario);
+		double salarioMitad = salario / 2;
+		System.out.println(salarioMitad);
+		//Ejemplo de double con división
+		double division = 1250 / 3;
+		System.out.println(division);
+
 	}
 }
