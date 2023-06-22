93a9b47d50c1664a31a5e82e2d697dd983d4f9ff
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-22 15:13:53 -0600

    Update TipoVariable.java

--- Java JRE y JDK: compile y ejecute su primer programa/TipoVariable.java
+++ Java JRE y JDK: compile y ejecute su primer programa/TipoVariable.java
@@ -2,7 +2,7 @@
 public class TipoVariable {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello WOrd");
+		System.out.println("Hello World");
 		
 		int edad = 25;
 		System.out.println("Mi edad es " + edad);
