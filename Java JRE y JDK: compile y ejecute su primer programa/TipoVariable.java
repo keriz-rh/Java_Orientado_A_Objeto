cf637bd84b185f83ab8764a6891d3154fdd08833
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-22 00:11:50 -0600

    Add files via upload

---
+++ Java JRE y JDK: compile y ejecute su primer programa/TipoVariable.java
@@ -0,0 +1,12 @@
+
+public class TipoVariable {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello WOrd");
+		
+		int edad = 25;
+		System.out.println("Mi edad es " + edad);
+		
+	
+	}
+}
