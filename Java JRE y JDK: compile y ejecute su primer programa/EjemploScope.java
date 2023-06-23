70a2ef42195bc62dccb514d3d33d3bca6fc27e5f
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-23 01:19:27 -0600

    Add files via upload

---
+++ Java JRE y JDK: compile y ejecute su primer programa/EjemploScope.java
@@ -0,0 +1,29 @@
+
+public class EjemploScope {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		//int edad = 17;
+		int edad =21;
+		int cantidadDePersonas = 2;
+		
+		boolean esPareja;
+		
+		if (cantidadDePersonas > 1) {
+			esPareja = true;
+		} else {
+			esPareja = false;
+		}
+		
+		boolean puedeEntrar = edad >= 18 && esPareja;
+		System.out.println("El valor de la operación es: " + esPareja);
+	
+			if (puedeEntrar) 
+			{
+			System.out.println("usted puede entrar.");
+		} else {
+			System.out.println("Usted no esta permitido a  " + "entrar.");
+		}
+
+	}
+}
