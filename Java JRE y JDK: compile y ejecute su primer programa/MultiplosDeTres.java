3b32232165ebd214c70170e00658de1e40c2b642
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-06-29 00:11:36 -0600

    Add files via upload

---
+++ Java JRE y JDK: compile y ejecute su primer programa/MultiplosDeTres.java
@@ -0,0 +1,13 @@
+
+//Usa un bucle for para imprimir todos los múltiplos de 3, entre 1 y 100.
+public class MultiplosDeTres {
+ public static void main(String[] args) {
+	for (int i = 1; i < 100; i++ )
+	{
+		if(i% 3 == 0) {
+			 System.out.println(i);
+
+		}
+	}	 
+}
+}
