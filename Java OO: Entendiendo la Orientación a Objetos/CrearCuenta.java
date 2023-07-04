9c5ac825279807c64f8ec04c79377c779d53a9d1
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-04 01:00:00 -0600

    Add files via upload

---
+++ Java OO: Entendiendo la Orientación a Objetos/CrearCuenta.java
@@ -0,0 +1,13 @@
+package proyecto_bytebank;
+
+	public class CrearCuenta {
+		public static void main(String[] args) {
+			Cuenta primeraCuenta = new Cuenta();
+			primeraCuenta.saldo = 1000;
+			
+			System.out.println(primeraCuenta.saldo);
+		}
+		
+	}
+
+
