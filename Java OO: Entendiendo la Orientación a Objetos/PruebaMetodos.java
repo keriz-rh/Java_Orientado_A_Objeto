3a6e69d7704452a4f8b1a4b28320d9b302b90ec8
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-04 23:51:51 -0600

    Add files via upload

---
+++ Java OO: Entendiendo la Orientación a Objetos/PruebaMetodos.java
@@ -0,0 +1,9 @@
+package proyecto_bytebank;
+
+public class PruebaMetodos {
+	public static void main(String[] args) {
+		Cuenta miCuenta = new cuenta();
+		miCuenta.saldo = 300;
+		miCuenta.depositar(valor)
+	}
+}
