04d75146efab4dc8245238063db2a648532128dd
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-05 00:12:34 -0600

    Update CrearCuenta.java

--- Java OO: Entendiendo la Orientación a Objetos/CrearCuenta.java
+++ Java OO: Entendiendo la Orientación a Objetos/CrearCuenta.java
@@ -2,12 +2,18 @@ package proyecto_bytebank;
 
 	public class CrearCuenta {
 		public static void main(String[] args) {
+			//vARIABLE			=	VALOR
 			Cuenta primeraCuenta = new Cuenta();
 			primeraCuenta.saldo = 1000;
-			
+			// primeraCuenta.pais = "peru"; NO COMPILA
 			System.out.println(primeraCuenta.saldo);
+			
+			Cuenta segundaCuenta = new Cuenta();
+			segundaCuenta.saldo = 500;
+			System.out.println(segundaCuenta.saldo);
+			System.out.println(primeraCuenta); //valor Default 0
+			
 		}
 		
 	}
-
 
