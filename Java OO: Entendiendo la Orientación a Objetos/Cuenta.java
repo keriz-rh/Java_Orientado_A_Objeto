eb639b2e41d4d421096149acfcb9543b18edebb2
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-05 01:06:55 -0600

    Update Cuenta.java

--- Java OO: Entendiendo la Orientación a Objetos/Cuenta.java
+++ Java OO: Entendiendo la Orientación a Objetos/Cuenta.java
@@ -6,7 +6,32 @@ public class Cuenta {
 	int numero;
 	String titular;
 	
+	//No retorna valor
 	void depositar(double valor) {
-		 saldo = saldo + valor;
+		//Esta cuenta		 Esta Cuenta
+		//This account		 this account
+		//This object
+		 this.saldo = this.saldo + valor;
+	}
+	
+	/*Siempre que se tenga declarar algún mensaje de "exito o error" 
+	podemos representarlo atraves de un booleano*/
+	
+	//Retorna valor
+	public boolean retirar(double valor){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} return false;
+	
+	}
+	
+	public boolean transferir(double valor, Cuenta cuenta){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			cuenta.depositar(valor);
+			return true;
+		} return false;
+		
 	}
 }
