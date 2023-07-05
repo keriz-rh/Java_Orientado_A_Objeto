26f7da4721efda2ec04660f4f4f6ed9dbe8f7d43
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-05 01:07:13 -0600

    Update PruebaMetodos.java

--- Java OO: Entendiendo la Orientación a Objetos/PruebaMetodos.java
+++ Java OO: Entendiendo la Orientación a Objetos/PruebaMetodos.java
@@ -8,5 +8,25 @@ public class PruebaMetodos {
 		miCuenta.depositar(200);
 		
 		System.out.println(miCuenta.saldo);
+		
+		miCuenta.retirar(100);
+		System.out.println(miCuenta.saldo);
+		
+		Cuenta cuentaJimena = new Cuenta();
+		cuentaJimena.depositar(1000);
+		boolean puedeTransferir = cuentaJimena.transferir(400, miCuenta);
+
+		
+		
+		if(puedeTransferir) {
+			System.out.println("Transferencia exitosa");
+		} else {
+			System.out.println("No se pudo transferir");
+		}
+		
+		System.out.println(cuentaJimena.saldo);
+		System.out.println(miCuenta.saldo);
+
+		
 	}
 }
