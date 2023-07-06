2de3828aa77e7686d3a7bf56d367f77547bc9a8a
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-06 02:11:34 -0600

    Add files via upload

---
+++ Java OO: Entendiendo la Orientación a Objetos/PruebaEncapsulamiento.java
@@ -0,0 +1,10 @@
+package proyecto_bytebank;
+
+public class PruebaEncapsulamiento {
+	public static void main(String[] args) {
+		Cuenta cuenta = new Cuenta();
+		Cliente cliente = new Cliente();
+		cliente.setNombre("Kevin");
+		cliente.setDocumento("sdaf232");		
+	}
+}
