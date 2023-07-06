2da79008eaeb3bd542df4f4a8c48033bdb539e05
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-06 00:43:23 -0600

    Update PruebaAcceso.java

--- Java OO: Entendiendo la Orientación a Objetos/PruebaAcceso.java
+++ Java OO: Entendiendo la Orientación a Objetos/PruebaAcceso.java
@@ -3,8 +3,11 @@ package proyecto_bytebank;
 public class PruebaAcceso {
 	public static void main(String[] args) {
 		Cuenta cuenta = new Cuenta();
+		cuenta.setAgencia(-22);
 		cuenta.depositar(400);
 		cuenta.retirar(300);
-		System.out.println(cuenta.obtenerSaldo());
+		System.out.println(cuenta.getSaldo());
+		System.out.println(cuenta.getAgencia());
+	
 	}
 }
