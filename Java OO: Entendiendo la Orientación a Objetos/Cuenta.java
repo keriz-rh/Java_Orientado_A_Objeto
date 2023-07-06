690736fc029752cf0efe5efed58191cbdd8954a6
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-05 23:37:43 -0600

    Update Cuenta.java

--- Java OO: Entendiendo la Orientación a Objetos/Cuenta.java
+++ Java OO: Entendiendo la Orientación a Objetos/Cuenta.java
@@ -1,37 +1,44 @@
 package proyecto_bytebank;
-
+/*public son llamados codificadores de acceso esto permite que
+ que sea visible desde cualquier parte del codigo
+ de igual manera tenemos al private, con esta no tenemos accso a la información*/
 public class Cuenta {
-	double saldo;
+	private double saldo;
 	int agencia;
 	int numero;
 	String titular;
-	
+
 	//No retorna valor
 	void depositar(double valor) {
 		//Esta cuenta		 Esta Cuenta
 		//This account		 this account
 		//This object
-		 this.saldo = this.saldo + valor;
+		this.saldo = this.saldo + valor;
 	}
-	
+
 	/*Siempre que se tenga declarar algún mensaje de "exito o error" 
 	podemos representarlo atraves de un booleano*/
-	
+
 	//Retorna valor
 	public boolean retirar(double valor){
 		if (this.saldo >= valor) {
-			this.saldo = this.saldo - valor;
+			this.saldo -= valor;
 			return true;
 		} return false;
-	
+
 	}
-	
+
 	public boolean transferir(double valor, Cuenta cuenta){
 		if (this.saldo >= valor) {
 			this.saldo = this.saldo - valor;
 			cuenta.depositar(valor);
 			return true;
 		} return false;
-		
+
+	}
+	
+	public double obtenerSaldo() {
+		return this.saldo;
 	}
 }
+
