a81a96389f288a27a4a3911bdda641d9858610a5
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-11 21:03:45 -0600

    Add files via upload

---
+++ Java Polimorfismo: Entendiendo herencia e interfaces/TestGerente.java
@@ -0,0 +1,15 @@
+package bytebanck_heredado;
+
+public class TestGerente {
+
+	public static void main(String[] args) {
+	/*	Gerente gerente = new Gerente();
+		gerente.setSalario(5000);*/
+		
+		Funcionario gerente = new Funcionario();
+		gerente.setSalario(6000);
+		gerente.setTipo(1);
+		
+		System.out.println(gerente.getBonificacion());
+	} 
+}
