fbe13a3db5d84cef426188ff143bfb44d4b1e1ae
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-11 20:29:24 -0600

    Update Funcionario.java

--- Java Polimorfismo: Entendiendo herencia e interfaces/Funcionario.java
+++ Java Polimorfismo: Entendiendo herencia e interfaces/Funcionario.java
@@ -5,4 +5,33 @@ public class Funcionario {
 	private String nombre;
 	private String documento; 
 	private double salario;
+	
+	/*Constructor es aquel metodo publico donde el tipo de objeto es la clase en sí.
+	 Y este puede o no recibir variables*/
+	public Funcionario() {
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDocumento() {
+		return documento;
+	}
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public double getBonificacion() {
+		return this.salario * 0.1;	
+	}
+
 }
