182e3923b7b0cbd38ac7fb2381fa52f506fbb623
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-13 12:50:33 -0600

    Update TestGerente.java

--- Java Polimorfismo: Entendiendo herencia e interfaces/TestGerente.java
+++ Java Polimorfismo: Entendiendo herencia e interfaces/TestGerente.java
@@ -3,13 +3,16 @@ package bytebanck_heredado;
 public class TestGerente {
 
 	public static void main(String[] args) {
-	/*	Gerente gerente = new Gerente();
-		gerente.setSalario(5000);*/
+	Gerente gerente = new Gerente();
+		//gerente.setSalario(5000);
 		
-		Funcionario gerente = new Funcionario();
+		//Funcionario gerente = new Funcionario();
 		gerente.setSalario(6000);
+		gerente.setclave("contraseña");
 		gerente.setTipo(1);
 		
 		System.out.println(gerente.getBonificacion());
+		System.out.println(gerente.iniciarSesion("contraseña"));
+
 	} 
 }
